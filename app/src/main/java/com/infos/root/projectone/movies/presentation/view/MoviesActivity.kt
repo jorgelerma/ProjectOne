11fb2375771ee4e66ebcec1e362430478e2a06fc
Jorge Lerma <jorge.lerma@globant.com>
2019-04-03 23:47:04 -0600

    Adds movie activity

---
+++ app/src/main/java/com/infos/root/projectone/movies/presentation/view/MoviesActivity.kt
@@ -0,0 +1,15 @@
+package com.infos.root.projectone.movies.presentation.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.infos.root.projectone.R
+
+
+
+class MoviesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies)
+    }
+}
--- app/src/main/java/com/infos/root/projectone/splash/presentation/SplashActivity.kt
+++ app/src/main/java/com/infos/root/projectone/splash/presentation/SplashActivity.kt
@@ -5,16 +5,19 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import com.infos.root.projectone.MainActivity
+import com.infos.root.projectone.movies.presentation.view.MoviesActivity
 import com.infos.root.projectone.R
 
 class SplashActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val intent = Intent(this, MainActivity::class.java)
+//        val intent = Intent(this, MainActivity::class.java)
+        val intent = Intent(this, MoviesActivity::class.java)
+
         Handler().postDelayed({
             startActivity(intent)
             Log.e(this.localClassName, " @ splash")
